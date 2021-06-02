a19afcedbad3410bf14f0b03f03516c6078b7b9b
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-06-02 12:55:27 +0530

    sparse true

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -13,7 +13,9 @@ const userSchema = mongoose.Schema(
     },
     email: {
       type: String,
+      index: true,
       unique: true,
+      sparse: true,
       // required: [true, "Every user must have an email"],
       // validate: [validator.isEmail, "Please provide an valid email."],
     },
