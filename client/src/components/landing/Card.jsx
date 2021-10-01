e28cb8d3b104fb87a38344f1acf51a54f982e88e
bhnprksh222 <bhnprksh222@gmail.com>
2021-10-02 00:21:50 +0530

    link fix

--- client/src/components/landing/Card.jsx
+++ client/src/components/landing/Card.jsx
@@ -2,6 +2,7 @@ import "./card.scss";
 import { Link } from "react-router-dom";
 const Card = (props) => {
   return (
+    <Link to={`/results/${props.obj[5].toLink}`} className="link">
     <div className="card">
       <div className="card_item">
         <img
@@ -35,11 +36,12 @@ const Card = (props) => {
         />
         <div className="card_item-name">{props.obj[3].name} </div>
       </div>
-      <Link to={`/results/${props.obj[5].toLink}`} className="card_title">
-        <div className="card_title-name">{props.obj[4].title}</div>
+      <div className="card_title">
+      <div className="card_title-name">{props.obj[4].title}</div>
         <span className="card_title-btn">&rarr;</span>
+      </div>
+      </div>
       </Link>
-    </div>
   );
 };
 export default Card;
